6cd6b7f5ee868f1a5ce2151a6f8877f4c7b6e199
Anant-Ranjan-Srivastava <anantranjansrivastava@gmail.com>
2026-05-09 15:17:38 +0530

    [SLICE 2]: Interaction between client and server.

--- client/client.go
+++ client/client.go
@@ -8,10 +8,28 @@ import(
 
 func main(){
 
+	// Connect to TCP server
 	client, error := net.Dial("tcp", "localhost:8000")
+	
 	if error != nil{
 		log.Fatalf("Connection failed: %v",error)
 	}
+	
 	defer client.Close()
+	
+	// Print successful connection.
 	fmt.Println("Successfully connected to the server !")
+
+	// Defining the message to be sent.
+	message := "Experimenting with sending raw bytes.\n"
+
+	// Converting and writing message bytes into TCP stream
+	_ , error = client.Write([]byte(message))
+
+	if error != nil{
+		log.Fatalf("Failed to send message: %v",error)
+	}
+
+	fmt.Println("Message sent to server")
+
 }
--- server/server.go
+++ server/server.go
@@ -8,19 +8,37 @@ import (
 
 func main(){
 
+	// TCP Listener
 	listener, error := net.Listen("tcp",":8000")
 	if error != nil {
 		log.Fatalf("Error starting server: %v",error)
 	}
 	defer listener.Close()
-
+	
 	fmt.Println("Server running on port 8080")
 
-	conn, error := listener.Accept()
+	// Accepting connections
+	connection, error := listener.Accept()
 	if error != nil{
 		log.Fatalf("Error accepting connection: %v", error)
 	} 
-	defer conn.Close()
+	defer connection.Close()
 
-	fmt.Println("Connection established from", conn.RemoteAddr())
+	// Printing established connection's IP.
+	fmt.Println("Connection established from", connection.RemoteAddr())
+
+	// Creating buffer for incoming data
+	buffer := make([]byte, 1024)
+
+	// Read bytes from TCP stream
+	bytesRead, error := connection.Read(buffer)
+	
+	if error != nil{
+		log.Fatalf("Failed to read data: %v",error)
+	}
+
+	// Converting received bytes into string
+	message := string(buffer[:bytesRead])
+
+	fmt.Printf("Received message: %s\n",message)
 }
